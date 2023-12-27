63d6501f9f01e104b3d0c784536ed1b7e1c50b90
Raja Jaiswal <143202200+rajajaiswal8414@users.noreply.github.com>
2023-12-27 23:33:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function evaluateExpression(expression) {
+  try {
+    return Function("return " + expression)();
+  } catch (error) {
+    return "Error";
+  }
+}
+
+let string = "";
+let buttons = document.querySelectorAll(".button");
+let inputField = document.getElementById("inputId");
+
+Array.from(buttons).forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let buttonText = e.target.innerHTML;
+
+    if (buttonText === "=") {
+      string = evaluateExpression(string);
+      inputField.value = string;
+    } else if (buttonText === "Ac") {
+      string = "";
+      inputField.value = string;
+    } else if (buttonText === "C") {
+      string = string.slice(0, -1);
+      inputField.value = string;
+    } else {
+      if (buttonText === "X") {
+        buttonText = "*";
+        string = string + buttonText;
+        inputField.value = string;
+      } else if (
+        (buttonText == "00" && string == "") ||
+        (buttonText == "0" && string == "")
+      ) {
+        string = "";
+      } else {
+        string = string + buttonText;
+        inputField.value = string;
+      }
+    }
+  });
+});
